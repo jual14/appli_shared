3bad262b230da5982a781c536ff18eb72d9a2ec0
julien <julien.alix14@gmail.com>
2017-04-02 15:34:28 +0200

    initializing firebase

---
+++ public/connect.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDwE2GhM1LoES6G7-ZsoZWPIoFOm4gzLi0",
+    authDomain: "appli-shared-6bf3d.firebaseapp.com",
+    databaseURL: "https://appli-shared-6bf3d.firebaseio.com",
+    projectId: "appli-shared-6bf3d",
+    storageBucket: "appli-shared-6bf3d.appspot.com",
+    messagingSenderId: "534072685753"
+  };
+  firebase.initializeApp(config);
